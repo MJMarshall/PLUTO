30aa2e37c12a83b2510ba88556949a8af88a151f
MJMarshall <MJMarshall@users.noreply.github.com>
2024-07-01 13:14:05 +0100

    Update cloudfunction.tf

--- terraform/cloudfunction.tf
+++ terraform/cloudfunction.tf
@@ -4,7 +4,7 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb = 256
   runtime             = "python39"
   source_repository {
-    url = "https://source.developers.google.com/projects/${var.project_id}/repos/github_mjmarshall_pluto/moveable-aliases/master/paths/cloudfunction/main.py"
+    url = "https://source.developers.google.com/projects/${var.project_id}/repos/github_mjmarshall_pluto/moveable-aliases/master/paths/cloudfunction"
   }
   entry_point       = "pubsub_to_bigquery"
   timeout = 60
